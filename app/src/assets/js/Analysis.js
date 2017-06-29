575cd14012cca12337db973c85ced020ea9a97b1
Filipe Voges <filipe.vogesh@gmail.com>
2017-06-29 09:48:32 -0300

    Iniciado a Busca pelos resultados

--- app/src/assets/js/Analysis.js
+++ app/src/assets/js/Analysis.js
@@ -1,1 +1,29 @@
 console.log("Analysis.js was loaded successfully");
+
+/*
+ * Responsible for creating a json with all results of the selected lottery
+ * @author Filipe Voges
+ * @returns {Void}
+ */
+function getDataResults(){
+    var i = 1;
+    var isFinal = false;
+    while (!isFinal) {
+        var url = 'https://www.lotodicas.com.br/api/mega-sena/'+ i;
+        $.getJSON(url , function(data) {
+            console.log(data);
+            isFinal = true;
+        });
+        i++;
+    }
+
+}
+
+/*
+ * Responsible for initiating the analysis of the results data to predict a possible future result
+ * @author Filipe Voges
+ * @returns {Void}
+ */
+function initAnalysis(){
+
+}
--- app/src/assets/js/controller.js
+++ app/src/assets/js/controller.js
@@ -1,1 +1,16 @@
 console.log("controller.js was loaded successfully");
+
+$(function() {
+    $(document).ready(function(){
+        $.ajax({
+            url:'./data/results.json',
+            type:'HEAD',
+            error: function(){
+                getDataResults();
+            },
+            success: function(){
+                initAnalysis();
+            }
+        });
+    });
+});
